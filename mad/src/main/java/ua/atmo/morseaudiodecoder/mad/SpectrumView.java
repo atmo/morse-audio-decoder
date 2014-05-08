b022fa747658fbe749d4a8c4bcb95e6c9458d83b
Stanislav Rogovskiy <stanislav.rogovskiy@gmail.com>
2014-05-08 13:07:11 +0300

    Fixed color calculating formula

--- mad/src/main/java/ua/atmo/morseaudiodecoder/mad/SpectrumView.java
+++ mad/src/main/java/ua/atmo/morseaudiodecoder/mad/SpectrumView.java
@@ -19,7 +19,6 @@ public class SpectrumView extends View {
         Display display = ((MainActivity)context).getWindowManager().getDefaultDisplay();
         width = display.getWidth();
         height = (int)(display.getHeight()*0.6);
-        height = (int)(display.getHeight()*0.6);
         paint = new Paint();
     }
 
@@ -29,7 +28,7 @@ public class SpectrumView extends View {
         int max = 600;
         int nrect = 100, h = 10, step = max/nrect;
         for (int i = 0; i<nrect; ++i) {
-            int color = getColor(i*step, max);
+            int color = getColor(i*step, max+1);
             paint.setARGB(255, Color.red(color), Color.green(color), Color.blue(color));
             canvas.drawRect(0, i*h, width, (i+1)*h, paint);
         }
@@ -38,13 +37,14 @@ public class SpectrumView extends View {
     private int getColor(int value, int max) {
         int step = max/(colormap.length-1);
         int idx = value/step;
-        int red = interpolate(Color.red(colormap[idx]), Color.red(colormap[idx+1]), value*255/max)/255,
-                green = interpolate(Color.green(colormap[idx]), Color.green(colormap[idx + 1]), value*255/max)/255,
-                blue = interpolate(Color.blue(colormap[idx]), Color.blue(colormap[idx + 1]), value*255/max)/255;
+        value -= step*idx;
+        int red = interpolate(Color.red(colormap[idx]), Color.red(colormap[idx+1]), value, step)/step,
+                green = interpolate(Color.green(colormap[idx]), Color.green(colormap[idx + 1]), value, step)/step,
+                blue = interpolate(Color.blue(colormap[idx]), Color.blue(colormap[idx + 1]), value, step)/step;
         return Color.rgb(red, green, blue);
     }
 
-    private int interpolate (int a, int b, int value) {
-        return (255 - value)*a + value*b;
+    private int interpolate (int a, int b, int value, int max) {
+        return (max - value)*a + value*b;
     }
 }
